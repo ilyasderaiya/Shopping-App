f1436c9c56dc3fb7e1625d3b6636366e392d6a77
Ilyas <Ilyasderaiya@gmail.com>
2019-03-16 15:43:15 -0400

    Sign Up View Controller

--- Shopping App/SignUpViewController.swift
+++ Shopping App/SignUpViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var txtFirstName: UITextField!
+    @IBOutlet weak var txtLastName: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtVerifyPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +23,20 @@ class SignUpViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func btnSignUpClick(_ sender: Any) {
+        if (txtFirstName.text?.isEmpty)! || (txtLastName.text?.isEmpty)! || (txtEmail.text?.isEmpty)! || (txtPassword.text?.isEmpty)! || (txtVerifyPassword.text?.isEmpty)! {
+            // Allert User to Enter All Details
+            return
+        }
+        
+        if ((txtPassword.text?.elementsEqual(txtVerifyPassword.text)) != true){
+            // Alert User Passwords are Not same
+            return
+        }
     }
-    */
+    
 
+    @IBAction func btnCancelClick(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
