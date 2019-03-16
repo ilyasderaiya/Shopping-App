cc5c5f65f3c65bc33bc728d5a5fdc359449c47d9
Ilyas <Ilyasderaiya@gmail.com>
2019-03-16 15:42:42 -0400

    SignIn View Controller

--- Shopping App/SignInViewController.swift
+++ Shopping App/SignInViewController.swift
@@ -10,21 +10,25 @@ import UIKit
 
 class SignInViewController: UIViewController {
 
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func btnSigninClick(_ sender: Any) {
     }
-    */
-
+    
+   
+    
+    @IBAction func btnSignUpClick(_ sender: Any) {
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let signupVC = sb.instantiateViewController(withIdentifier: "SignUpViewController") as! SignUpViewController
+        self.present(signupVC, animated: true)
+    }
+    
 }
