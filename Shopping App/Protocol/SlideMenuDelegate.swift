1dfe9fa3f2208eeb36f87bd6995529133f00dd5a
Emil <emilabrahamz@gmail.com>
2019-03-23 10:21:47 -0400

    Protocol for Slide Menu

---
+++ Shopping App/Protocol/SlideMenuDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SlideMenuDelegate.swift
+//  Shopping App
+//
+//  Created by Ilyas Deraiya on 2019-03-22.
+//  Copyright © 2019 Ilyas. All rights reserved.
+//
+
+import Foundation
+
+protocol SlideMenuDelegate {
+    func slideMenuItemSelectedAtIndex(_ index : Int32)
+}
