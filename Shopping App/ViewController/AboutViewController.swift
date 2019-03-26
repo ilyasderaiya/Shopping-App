2fb148994d2e1bc0673d27982c7a10654276f734
MacStudent <macstudent@126-imac22.local>
2019-03-26 18:23:57 -0400

    contact us

--- Shopping App/ViewController/AboutViewController.swift
+++ Shopping App/ViewController/AboutViewController.swift
@@ -16,10 +16,9 @@ class AboutViewController: BaseViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "About-Us"
+        self.navigationItem.rightBarButtonItem(UIBarButtonItem(title: "Contact US", style: UIBarButtonItem.Style.plain, target: self, action: #selector(AboutViewController.launchAlert)))
         addCartButton()
         addSlideMenuButton()
-        
-        
         wkAbout.navigationDelegate = self
         loadHtml()
     }
@@ -29,6 +28,13 @@ class AboutViewController: BaseViewController, WKNavigationDelegate {
         let localFilePath = Bundle.main.url(forResource: "About", withExtension: "html")
         let myReq = URLRequest(url: localFilePath!)
         wkAbout.load(myReq)
+    }
+    func launchAlert(){
+        let contactInfo="email:\nemilabrahamz@gmail.com\nsrinathsuddala@gmail.com\nilyasderaiya@gmail.com"
+        let alert = UIAlertController(title: "Contact US", message:contactInfo, preferredStyle: .alert)
+        let okbutton = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okbutton)
+        self.present(alert, animated: true)
     }
 
 }
